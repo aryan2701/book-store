90d443a4fad24fee58b91195f7e494e2de88fff9
Aryan Raghuvanshi <124778245+AryanRaghuvanshi09@users.noreply.github.com>
2024-10-24 19:08:04 +0530

    Update db.js

--- Stationary/backend/config/db.js
+++ Stationary/backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // MongoDB connection URI
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/bookstore'; // Use your MongoDB URI here
+const MONGO_URI = process.env.MONGO_URI ; // Use your MongoDB URI here
 
 // Connect to MongoDB
 const connectDB = async () => {
