c8fc363a268a6dd73e8b779d22ab4fec81d51d92
Aryan Raghuvanshi <124778245+AryanRaghuvanshi09@users.noreply.github.com>
2024-10-24 16:18:02 +0530

    Update and rename Customers.js to Admin.js

---
+++ Stationary/frontend/src/pages/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminDash from '../components/AdminDash';
+
+const Admin = () => {
+    return (
+        <div>
+            
+            <AdminDash />
+        </div>
+    );
+};
+
+export default Admin;
